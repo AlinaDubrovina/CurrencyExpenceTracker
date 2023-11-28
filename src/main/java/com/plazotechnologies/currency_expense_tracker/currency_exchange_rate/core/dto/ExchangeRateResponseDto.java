d2b6e57e1a873a424a76773eae19fda93fad87e1
Alina Dubrovina <dubrovinaa389@gmail.com>
2023-11-28 14:29:20 +0100

    fix: add AllArgsConstructors

--- src/main/java/com/plazotechnologies/currency_expense_tracker/currency_exchange_rate/core/dto/ExchangeRateResponseDto.java
+++ src/main/java/com/plazotechnologies/currency_expense_tracker/currency_exchange_rate/core/dto/ExchangeRateResponseDto.java
@@ -2,12 +2,16 @@ package com.plazotechnologies.currency_expense_tracker.currency_exchange_rate.co
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@NoArgsConstructor
 public class ExchangeRateResponseDto {
     @JsonProperty("meta")
     private CurrencyPairDto meta;
--- src/main/java/com/plazotechnologies/currency_expense_tracker/currency_exchange_rate/core/dto/ExchangeRateValueDto.java
+++ src/main/java/com/plazotechnologies/currency_expense_tracker/currency_exchange_rate/core/dto/ExchangeRateValueDto.java
@@ -1,10 +1,14 @@
 package com.plazotechnologies.currency_expense_tracker.currency_exchange_rate.core.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@NoArgsConstructor
 public class ExchangeRateValueDto {
     private String datetime;
     private String close;
