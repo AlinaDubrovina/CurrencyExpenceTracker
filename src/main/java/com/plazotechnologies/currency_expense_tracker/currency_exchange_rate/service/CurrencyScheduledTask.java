223b15d7ca309cbffe073106a7ba843fbfc8f836
Alina Dubrovina <dubrovinaa389@gmail.com>
2023-11-28 16:23:30 +0100

    refactored: changed from using class to interface in parameters

--- src/main/java/com/plazotechnologies/currency_expense_tracker/currency_exchange_rate/service/CurrencyScheduledTask.java
+++ src/main/java/com/plazotechnologies/currency_expense_tracker/currency_exchange_rate/service/CurrencyScheduledTask.java
@@ -1,16 +1,18 @@
 package com.plazotechnologies.currency_expense_tracker.currency_exchange_rate.service;
 
+import com.plazotechnologies.currency_expense_tracker.currency_exchange_rate.dao.IExchangeRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class CurrencyScheduledTask {
-
-    private final ExchangeRateService exchangeRateService;
+    private final IExchangeRateService exchangeRateService;
 
     @Autowired
-    public CurrencyScheduledTask(ExchangeRateService exchangeRateService) {
+    public CurrencyScheduledTask(IExchangeRateService exchangeRateService) {
         this.exchangeRateService = exchangeRateService;
     }
 
